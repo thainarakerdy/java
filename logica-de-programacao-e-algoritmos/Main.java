d5892170b66ee8ad9c2f0702dd2762e327e8287c
thainarakerdy <thainarakerdy@gmail.com>
2026-04-11 21:58:32 -0300

    Initial commit

---
+++ logica-de-programacao-e-algoritmos/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        double x;
+        double y;
+        double z;
+        double soma;
+ System.out.println("Hello World");
+
+        x =  111;
+        y =  75;
+        z = 85;
+        soma = (x - y) * z;
+
+        System.out.println("Soma = " + soma);
+    }
+}
